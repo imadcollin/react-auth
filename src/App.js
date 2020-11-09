972d3bf8d8bc0eded7bde8cd4baafe2260ab994b
imadcollin <imad.collin@gmail.com>
2020-11-09 14:47:35 +0100

    Add initial protect route

--- src/App.js
+++ src/App.js
@@ -1,12 +1,53 @@
 import Login from "./Components/login";
 import Logout from "./Components/logout";
 
+import {
+  BrowserRouter as Router,
+  Route,
+  Link,
+  Redirect,
+} from "react-router-dom";
+
 function App() {
+  function Home() {
+    return <h2>Public</h2>;
+  }
+
+  function Protect() {
+    return <h2>Protected</h2>;
+  }
+
+  const PrivateRoute = ({ component: Component, ...rest }) => (
+    <Route {...rest} render={(props) => <Redirect to="/login" />} />
+  );
+
   return (
     <div className="App">
       <h1>React Auth</h1>
-      <Login></Login>
-      <Logout></Logout>
+      <Router>
+        <div>
+          <nav>
+            <ul>
+              <li>
+                <Link to="/home">Public</Link>
+              </li>
+              <li>
+                <Link to="/login">Login</Link>
+              </li>
+              <li>
+                <Link to="/protect">Protected</Link>
+              </li>
+            </ul>
+          </nav>
+
+          <Route path="/home" component={Home} />
+          <Route path="/login" component={Login} />
+          <PrivateRoute path="/protect" component={Protect} />
+        </div>
+      </Router>
+
+      {/* <Login></Login>
+      <Logout></Logout> */}
     </div>
   );
 }
